8d0dcb94ef7c5aa18cff18ad8b96852cc740e707
ChillYall <mariellehoneypchiong@gmail.com>
2022-09-27 17:42:19 +0800

    added css styles, js, class names and linked css

---
+++ lesson2/scripts/myscripts.js
@@ -0,0 +1,12 @@
+var today = new Date();
+var dd = String(today.getDate()).padStart(2, '0');
+var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+var yyyy = today.getFullYear();
+var objToday = new Date(),
+	curHour = objToday.getHours() > 12 ? objToday.getHours() - 12 : (objToday.getHours() < 10 ? "0" + objToday.getHours() : objToday.getHours()),
+	curMinute = objToday.getMinutes() < 10 ? "0" + objToday.getMinutes() : objToday.getMinutes(),
+	curSeconds = objToday.getSeconds() < 10 ? "0" + objToday.getSeconds() : objToday.getSeconds(),
+	curMeridiem = objToday.getHours() > 12 ? "PM" : "AM";
+var today = mm + "/" + dd + "/" + yyyy + " " + curHour + ":" + curMinute + ":" + curSeconds + curMeridiem;
+
+document.getElementById("currentdate").textContent = today;
