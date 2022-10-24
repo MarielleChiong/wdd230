4b06a3b78ebcafb16007744932b04f44cba7a765
ChillYall <mariellehoneypchiong@gmail.com>
2022-10-24 11:23:00 +0800

    fixed js

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -1,20 +1,20 @@
-const tC = 10   ;
-const skmH = 5;
+const tempC = 10   ;
+const speedkh = 5;
 
-document.querySelector('.degrees').textContent = tC;
-document.querySelector('.speed').textContent = skmH;
+document.querySelector('.degrees').textContent = tempC;
+document.querySelector('.speed').textContent = speedkh;
 
-const tF = tC * (9/5) + 32;
-const smH = skmH / 1.609;
+const tempF = tempC * (9/5) + 32;
+const speedmH = speedkh / 1.609;
 
-console.log(tF);
-console.log(smH);
+console.log(tempF);
+console.log(speedmH);
 
 
-if (tF <= 50 && smH > 3) {
-const f = 35.74 + 0.6215 * tF - 35.75 * (smH**0.16) + 0.4275 * tF * (smH**0.16)
-document.querySelector('.wind').textContent = f.toFixed(1);
-}
+if (tempF <= 50 && speedmH > 3) {
+    const f = 35.74 + 0.6215 * tempF - 35.75 * (speedmH ** 0.16) + 0.4275 * tempF * (speedmH ** 0.16)
+    document.querySelector('.wind').textContent = f.toFixed(1);
+    }
 else {
   document.querySelector('.wind').textContent = 'N/A';
 }
