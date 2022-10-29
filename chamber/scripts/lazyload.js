6b43efd88c1e7c53d8a2edef9a90de347ce5c762
ChillYall <mariellehoneypchiong@gmail.com>
2022-10-30 00:03:00 +0800

    discover page image section (not finished)

---
+++ chamber/scripts/lazyload.js
@@ -0,0 +1,32 @@
+let imagesLoading = document.querySelectorAll("data-src");
+
+const imageOptions = {
+    threshold: 0,
+    rootMargin: "0 0 50px 0"
+};
+
+const loadImg = (image) => {
+  image.setAttribute("src", image.getAttribute("data-src"));
+  image.onload = () => {
+    image.removeAttribute("data-src");
+  };
+};
+
+if ("IntersectionObserver" in window) {
+    const observer = new IntersectionObserver((items, observer) => {
+      items.forEach((item) => {
+        if (item.isIntersecting) {
+          loadImg(item.target);
+          observer.unobserve(item.target);
+        }
+      });
+    }, imageOptions);
+    imagesLoading.forEach((img) => {
+      observer.observe(img);
+    });
+  } 
+else {
+    imagesLoading.forEach((img) => {
+      loadImg(img);
+    });
+  }
